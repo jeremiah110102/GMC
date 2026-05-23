fcdca44582e6d4f84bb1bb88b6ef4e489fb0358d
jeremiah110102 <macailaojeremiah3@gmail.com>
2026-05-23 23:26:32 +0800

    added number

--- js/dashboard.js
+++ js/dashboard.js
@@ -303,7 +303,9 @@ const ContentModerator = {
   // =========================
   'http://', 'https://', 'www.',
   '.com', '.net', '.org',
-  '@gmail', '@yahoo', '@hotmail'
+  '@gmail', '@yahoo', '@hotmail',
+  //number
+  '1','2','3','4','5','6','7','8','9','0'
 ],
 
     suspiciousPatterns: [
